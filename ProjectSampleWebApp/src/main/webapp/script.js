92d1c5a7275909ccb23b1e30ed231479f28707ee
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2022-12-05 11:08:42 +0100

    Javascript Search
    Search with javscript

--- ProjectSampleWebApp/src/main/webapp/script.js
+++ ProjectSampleWebApp/src/main/webapp/script.js
@@ -1,3 +1,43 @@
-/**
- * 
- */
+function init() {
+    // initialize the listener for the search button and trigger the search, when clicked
+    document.getElementById("searchbutton").addEventListener('click', startSearch);
+}
+
+
+async function startSearch() {
+    // get the search term from the searchfield
+    let term = document.getElementById("searchfield").value;
+    console.log(term);
+    // call our servlet (this is an asynchronous call, so we use await)
+    const response = await fetch('./search?term='+term);
+    const results = await response.json();
+    // here we get our JSON from the servlet
+    console.log(results);
+    // display the results
+    displaySearchResult(results);
+}
+
+function displaySearchResult(searchResults) {
+    // get the element from html where we want to display the results
+    let resultElement = document.getElementById("results");
+    resultElement.innerHTML = '';
+    let titleElement = document.createElement("h2");
+    titleElement.innerHTML = "Results ("+searchResults.length+")";
+    resultElement.appendChild(titleElement);
+
+    // iterate over all results
+    for (let i = 0; i < searchResults.length; i++) {
+        let entry = searchResults[i];
+        // create the html output for the person
+        let personDiv = document.createElement("div");
+        let nameElement = document.createElement("p");
+        
+        // insert the values from json
+        nameElement.innerHTML = entry.firstName +", "+entry.lastName;
+        nameElement.classList.add("largetext");
+        
+        // combine the elements and append to the results
+        personDiv.appendChild(nameElement);
+        resultElement.appendChild(personDiv);
+    }
+}
